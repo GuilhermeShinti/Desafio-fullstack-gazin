cc319d12614f56f8a08635c2de0051289b68d9db
GuilhermeShinti <guilhermeshinti@gmail.com>
2021-12-10 15:09:48 -0300

    adding cors

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
 import "express-async-errors";
 
 import "reflect-metadata";
@@ -12,6 +13,7 @@ createConnection();
 const app = express();
 
 app.use(express.json())
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
