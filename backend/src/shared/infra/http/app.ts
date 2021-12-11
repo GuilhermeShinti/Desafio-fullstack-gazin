c99b7fa96d39d040123e14516b5190e41515d639
GuilhermeShinti <guilhermeshinti@gmail.com>
2021-12-11 15:12:42 -0300

    configuring swagger

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import "express-async-errors";
+import swaggetUi from "swagger-ui-express";
+
+import swaggerFile from "../../../swagger.json";
 
 import "reflect-metadata";
 
@@ -12,8 +15,9 @@ import { AppError } from "../../errors/AppError";
 createConnection();
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
+app.use("/api-docs", swaggetUi.serve, swaggetUi.setup(swaggerFile));
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
